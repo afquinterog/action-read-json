91386ff916d5c94fb03adfe093a7223462071457
Andres Quintero <aquintero@oru.com.co>
2020-05-19 21:50:18 -0500

    test

--- dist/index.js
+++ dist/index.js
@@ -62,7 +62,11 @@ const { promises: fs } = __webpack_require__(747)
 const main = async () => {
   const path = core.getInput('path')
   const content = await fs.readFile(path, 'utf8')
-  core.setOutput('content', content)
+  //var json = parser.toJson(content);
+  tag = content.match(/<version>([^<]*)<\/version>/)
+  //content = content[1]
+  version = tag[1]
+  core.setOutput('content', version)
 }
 
 main().catch(err => core.setFailed(err.message))
--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const { promises: fs } = require('fs')
 
 const main = async () => {
   const path = core.getInput('path')
-  //const content = await fs.readFile(path, 'utf8')
+  const content = await fs.readFile(path, 'utf8')
   //var json = parser.toJson(content);
-  //tag = content.match(/<version>([^<]*)<\/version>/)
+  tag = content.match(/<version>([^<]*)<\/version>/)
   //content = content[1]
-  //version = tag[1]
-  core.setOutput('content', 'hi')
+  version = tag[1]
+  core.setOutput('content', version)
 }
 
 main().catch(err => core.setFailed(err.message))
