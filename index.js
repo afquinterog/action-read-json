931f20d35a09724dd297f84d39e4153a291129fb
Andres Quintero <aquintero@oru.com.co>
2020-05-19 21:18:29 -0500

    Test

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const main = async () => {
   const path = core.getInput('path')
   const content = await fs.readFile(path, 'utf8')
   //var json = parser.toJson(content);
-  content = content.match(/<version>([^<]*)<\/version>/)
-  content = content[1]
+  //tag = content.match(/<version>([^<]*)<\/version>/)
+  //content = content[1]
   //version = tag[1]
   core.setOutput('content', 'hi')
 }
