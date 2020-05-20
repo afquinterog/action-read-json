a94588efc9ce28bbd2fb64d7381af2e7049f6c60
Andres Quintero <aquintero@oru.com.co>
2020-05-20 11:04:27 -0500

    read file

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 'use strict'
 
 const core = require('@actions/core')
+const xml2js = require('xml2js');
 const { promises: fs } = require('fs')
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
 
 const main = async () => {
   const path = core.getInput('path')
   const content = await fs.readFile(path, 'utf8')
+  parser.parseString(content, function(error, result) {
+    if(error === null) {
+        core.setOutput('content', result)
+    }
+  });
   //var json = parser.toJson(content);
-  const tag = /<version>(.*?)<\/version>/.exec(content)
+  //const tag = /<version>(.*?)<\/version>/.exec(content)
   //content = content[1]
   //version = tag[1]
-  core.setOutput('content', tag)
+  //core.setOutput('content', tag)
 }
 
 main().catch(err => core.setFailed(err.message))
